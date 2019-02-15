30fb69e0f2cabbb01d3933194a812fbcb9ae5271
Neil Winton <nwinton@pivotal.io>
2019-02-15 11:35:15 +0530

    Rewrote script to handle services using credhub-provided credentials and made more robust.

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -1,22 +1,59 @@
 #!/usr/bin/env bash
 set -e
 
+app_name="${1:-pal-tracker}"
+script_dir="${2:-.}"
+service_name="${3:-tracker-database}"
 
-app_guid=`cf app $1 --guid`
-credentials=`cf curl /v2/apps/$app_guid/env | jq '.system_env_json.VCAP_SERVICES | .[] | .[] | select(.instance_name=="tracker-database") | .credentials'`
+function pre_tunnel_exit() {
+    echo "ERROR: No suitable credentials found for application '$app_name' and service '$service_name'" >&2
+}
 
-ip_address=`echo $credentials | jq -r '.hostname'`
-db_name=`echo $credentials | jq -r '.name'`
-db_username=`echo $credentials | jq -r '.username'`
-db_password=`echo $credentials | jq -r '.password'`
+trap pre_tunnel_exit EXIT
 
-echo "Opening ssh tunnel to $ip_address"
-cf ssh -N -L 63306:$ip_address:3306 pal-tracker &
+echo "Retrieving target database parameters ..."
+
+# The value of VCAP_SERVICES is obtained from the running process
+# environment to handle the use of credhub-managed credentials.
+vcap_services=$(cf ssh $app_name -c 'perl -0 -ne "print if (s/^VCAP_SERVICES=//)" /proc/$(pgrep java)/environ')
+credentials=$(echo "$vcap_services" | jq ".[] | .[] | select(.instance_name == \"$service_name\") | .credentials")
+
+db_host=$(echo $credentials | jq -r '.hostname')
+db_name=$(echo $credentials | jq -r '.name')
+db_username=$(echo $credentials | jq -r '.username')
+db_password=$(echo $credentials | jq -r '.password')
+db_port=$(echo $credentials | jq -r '.port')
+
+test -n "$db_host" || exit 1
+
+echo "Opening ssh tunnel to $db_host:$db_port"
+cf ssh -N -L 63306:$db_host:$db_port $app_name &
 cf_ssh_pid=$!
 
-echo "Waiting for tunnel"
-sleep 5
+function close_tunnel_at_exit() {
+    echo "Closing tunnel"
+    kill -KILL $cf_ssh_pid
+}
 
-flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" -locations=filesystem:$2/databases/tracker -user=$db_username -password=$db_password migrate
+trap close_tunnel_at_exit EXIT
 
-kill -STOP $cf_ssh_pid
+# Note that the following depends on /dev/tcp support being compiled
+# into the version of bash being used. However, if such support is
+# not present it will gracefully degrade into a 20-second wait, which
+# should be sufficient.
+tries=20
+while (( tries > 0 )) && ! (echo "" > /dev/tcp/localhost/63306) 2> /dev/null
+do
+    echo "Waiting for tunnel ($tries attempts remaining)"
+    (( tries = tries - 1 ))
+    sleep 1
+done
+
+echo "Running migration ..."
+flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" \
+    -locations=filesystem:"$script_dir"/databases/tracker \
+    -user="$db_username" \
+    -password="$db_password" \
+    migrate
+
+echo "Migration complete"
