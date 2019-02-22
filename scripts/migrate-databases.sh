89aee8c6fdd38c0c540b76a70c59389cd865c982
Neil Winton <nwinton@pivotal.io>
2019-02-22 14:36:16 +0530

    Rework script to use credentials from a service key.

--- scripts/migrate-databases.sh
+++ scripts/migrate-databases.sh
@@ -4,6 +4,7 @@ set -e
 app_name="${1:-pal-tracker}"
 script_dir="${2:-.}"
 service_name="${3:-tracker-database}"
+service_key="${4:-cf-mysql}"   # Default to same key-name as 'cf mysql'
 
 function pre_tunnel_exit() {
     echo "ERROR: No suitable credentials found for application '$app_name' and service '$service_name'" >&2
@@ -11,12 +12,18 @@ function pre_tunnel_exit() {
 
 trap pre_tunnel_exit EXIT
 
+# If a service is using credhub then the credentials are not exposed
+# in VCAP_SERVICES. So we create a service key for the database in order
+# to obtain the necessary. Creating a key is idempotent, so it does not
+# matter if it already exists.
+
+echo "Creating service key, if necessary ..."
+
+cf create-service-key $service_name $service_key > /dev/null
+
 echo "Retrieving target database parameters ..."
 
-# The value of VCAP_SERVICES is obtained from the running process
-# environment to handle the use of credhub-managed credentials.
-vcap_services=$(cf ssh $app_name -c 'perl -0 -ne "print if (s/^VCAP_SERVICES=//)" /proc/$(pgrep java)/environ')
-credentials=$(echo "$vcap_services" | jq ".[] | .[] | select(.instance_name == \"$service_name\") | .credentials")
+credentials=$(cf service-key $service_name $service_key | sed -ne '/{/,$p')
 
 db_host=$(echo $credentials | jq -r '.hostname')
 db_name=$(echo $credentials | jq -r '.name')
@@ -26,7 +33,8 @@ db_port=$(echo $credentials | jq -r '.port')
 
 test -n "$db_host" || exit 1
 
-echo "Opening ssh tunnel to $db_host:$db_port"
+echo "Opening ssh tunnel to $db_host:$db_port ..."
+
 cf ssh -N -L 63306:$db_host:$db_port $app_name &
 cf_ssh_pid=$!
 
@@ -41,6 +49,7 @@ trap close_tunnel_at_exit EXIT
 # into the version of bash being used. However, if such support is
 # not present it will gracefully degrade into a 20-second wait, which
 # should be sufficient.
+
 tries=20
 while (( tries > 0 )) && ! (echo "" > /dev/tcp/localhost/63306) 2> /dev/null
 do
@@ -50,6 +59,7 @@ do
 done
 
 echo "Running migration ..."
+
 flyway-*/flyway -url="jdbc:mysql://127.0.0.1:63306/$db_name" \
     -locations=filesystem:"$script_dir"/databases/tracker \
     -user="$db_username" \
